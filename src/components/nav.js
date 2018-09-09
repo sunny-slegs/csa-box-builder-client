db4016e33d0bd0bdaef717292eae114a9e187609
Sonya <sonjay08@gmail.com>
2018-09-09 10:25:30 -0500

    deployed with zeit

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Redirect} from 'react-router-dom';
 import {goHome} from '../actions/boxes';
 import {showAbout} from '../actions/boxes';
 import {clearAuth} from '../actions/auth';
